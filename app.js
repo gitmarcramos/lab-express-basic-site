f08cb8536261e25bc02c1a383f76c6713e7004e4
Marc Ramos <59534439+gitmarcramos@users.noreply.github.com>
2021-10-11 17:55:20 +0200

    working on different stuff

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
+//! dependencies
 const express = require("express");
 const app = express();
 const hbs = require("hbs");
-const port = 5555;
+const port = 5550;
 
+const db = require("./public/js/db.js");
+
+//! base setup
+// for the HBS views
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 
-//! render static folders:
+//to use the public folder with static files
 app.use(express.static(__dirname + "/public"));
 
-//render the views:
+//! render the views:
+// HOME PAGE
 app.get(["/", "/home"], (req, res) => {
-  res.render("home.hbs");
+  const navElements = ["Home", "About the band", "Work"];
+
+
+
+  res.render("home.hbs", {
+    pageTitle: "Home",
+    navElements,
+    db: {
+      name: "Home",
+      
+    }
+  });
 });
 
-app.get("/about", (req, res, next) => {
-  res.render("about.hbs");
-});
 
+// ABOUT PAGE:
+app.get('/about', (rer, res, next)=>{
+  res.render('about.hbs')
+})
+
+// WORK PAGE
 app.get("/work", (req, res, next) => {
-  res.render("work.hbs");
+  res.render("work.hbs", {
+    pageTitle: "Work",
+  });
 });
+
+
+// BAND PAGE
+app.get("/:linkname", (req, res, next) => {
+  res.render("band.hbs", {
+    pageTitle: "Band page",
+    db
+  });
+});
+
 
 
 
---
+++ public/js/db.js
@@ -0,0 +1,32 @@
+const db = [
+  {
+    name: "Queen",
+    members: [
+      { name: "Freddie Mercury", position: "Lead vocals" },
+      { name: "Brian May", position: "Lead guitar, vocals" },
+      { name: "John Deacon", position: "Bass guitar, vocals" },
+      { name: "Roger Taylor", position: "Drums, vocals" },
+    ],
+    albums: [
+      { name: "Queen", released: 1973 },
+      { name: "Queen II", released: 1974 },
+      { name: "Sheer Heart Attack", released: 1974 },
+      { name: "A night at the Opera", released: 1975 },
+    ],
+  },
+  {
+    name: "Led Zeppelin",
+    members: [
+      { name: "Freddie Mercury", position: "Lead vocals" },
+      { name: "Brian May", position: "Lead guitar, vocals" },
+      { name: "John Deacon", position: "Bass guitar, vocals" },
+      { name: "Roger Taylor", position: "Drums, vocals" },
+    ],
+    albums: [
+      { name: "Queen", released: 1973 },
+      { name: "Queen II", released: 1974 },
+      { name: "Sheer Heart Attack", released: 1974 },
+      { name: "A night at the Opera", released: 1975 },
+    ],
+  },
+];
